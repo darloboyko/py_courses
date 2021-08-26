580ec44182a85de3be8430faa55edf8a6b44113c
Daria Loboiko <darloboyko@gmail.com>
2021-08-26 21:14:55 +0300

    py_courses

--- homeworks/home_work_3/task_02.py
+++ homeworks/home_work_3/task_02.py
@@ -5,9 +5,8 @@
 k1 = float(input("Enter k1:  "))
 k2 = float(input("Enter k2:  "))
 
-s = 0.5*(k1 * k2)
-p = k1 + k2 + (k1 + k2)**1/2
-
+s = round(0.5*(k1 * k2), 1)
+p = round(k1 + k2 + (k1 + k2)**1/2, 2)
 
 print("Squares", s, "mm2")
 
