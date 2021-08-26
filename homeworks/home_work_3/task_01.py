eff612fde020b4a23c1c3ca337c5e9a3a433f7e4
Daria Loboiko <darloboyko@gmail.com>
2021-08-26 21:02:38 +0300

    py_courses

--- homeworks/home_work_3/task_01.py
+++ homeworks/home_work_3/task_01.py
@@ -4,10 +4,12 @@
 import time
 
 current_time_millis = round(time.time()* 1000)
+day = current_time_millis // (3600*1000*24)
+hours = (current_time_millis % (3600*1000*24)) // (3600*1000)
+minute = (current_time_millis % ((day*24 + hours)*3600*1000)) // (60*1000)
+seconds =  (current_time_millis % ((day*24*3600 + hours*3600 + minute*60)*1000)) // (1000)
 
-a = time.strftime("%d:%H:%M:%S", time.localtime())
-
-print(a)
+print(day,":",hours,":",minute,":",seconds)
 print("Current time in millis:", current_time_millis)
 
 
