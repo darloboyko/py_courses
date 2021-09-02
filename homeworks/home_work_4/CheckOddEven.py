2d7f5fe49a9ce3567ca32f3914c3eced74043cbb
Daria Loboiko <darloboyko@gmail.com>
2021-09-02 07:18:47 +0300

    py_courses

---
+++ homeworks/home_work_4/CheckOddEven.py
@@ -0,0 +1,13 @@
+#Написать программу с названием “CheckOddEven”, 
+# которая печатает “Odd Number” если переменная “number” 
+# типа int нечетная. Или “Even Number” если переменная четная. 
+# Программа всегда должна перед выходом печатать “BYE”
+
+number = int(input("number:  "))
+
+if number%2 == 1:
+    print("Odd Number")
+else:
+ print("Even Number")  
+ 
+print("BYE")
---
+++ homeworks/home_work_4/PrintNumberInWord.py
@@ -0,0 +1,26 @@
+#Написать программу с названием “PrintNumberInWord”, 
+# которая напечатает “ONE”, “TWO”, …, “NINE”, 
+# “OTHER” если переменная “number” типа int будет 1, 2, 3, 4, … 9, или любой другой.
+
+number = int(input("number:  "))
+
+if number == 1:
+ print("ONE")
+elif number == 2:
+ print("TWO")
+elif number == 3:
+ print("THREE")
+elif number == 4:
+ print("FOUR")
+elif number == 5:
+ print("FIVE")
+elif number == 6:
+ print("SIX")
+elif number == 7:
+ print("SEVEN")
+elif number == 8:
+ print("EIGHT")
+elif number == 9:
+ print("NINE")
+else:
+ print("OTHER")
