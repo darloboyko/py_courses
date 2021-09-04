d896bb3895ee26c90f2a8357e7a86ab9cf890e4a
Daria Loboiko <darloboyko@gmail.com>
2021-09-04 12:34:55 +0300

    py_courses

---
+++ lectures/lectures_05/Grade.py
@@ -0,0 +1,19 @@
+
+scores = int(input("Enter scores:  "))
+
+if  90 <= scores <= 100:
+    print("Grade: A")
+
+elif  80 <= scores < 90:
+    print("Grade: B")
+
+elif  70 <= scores < 80:
+    print("Grade: C")
+
+elif  60 <= scores < 70:
+    print("Grade: D")
+
+else:
+   print("Grade: F")
+
+
