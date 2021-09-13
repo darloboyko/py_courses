c226a503de677f115eaf6d8f070eb165c56ab3ea
Daria Loboiko <darloboyko@gmail.com>
2021-09-13 19:06:17 +0300

    py_courses

--- homeworks/home_work_7/WordsInReverse.py
+++ homeworks/home_work_7/WordsInReverse.py
@@ -3,6 +3,6 @@
 #Ответ: "battle no requires which that is victory greatest The"
 #Строку ввести с клавиатуры
 
-s = input("Enter string: ").replace('"', "").split(" ")[::-1]
+s = input("Enter string: ").replace('"', "").split()[::-1]
 s_lst =' '.join(str(i) for i in s)
 print(f'"{s_lst}"')
