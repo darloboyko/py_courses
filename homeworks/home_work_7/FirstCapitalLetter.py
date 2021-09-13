8715c5c518f98ea0d7970ea98d9bd7c56f0131e2
Daria Loboiko <darloboyko@gmail.com>
2021-09-13 18:55:58 +0300

    py_courses

---
+++ homeworks/home_work_7/FirstCapitalLetter.py
@@ -0,0 +1,7 @@
+#2. Написать программу, которая возьмет строку и в каждом слове заменит первую букву на заглавную
+#Строку ввести с клавиатуры
+#Пример: "This is some test STRING"
+#Ответ: "This Is Some Test String"
+
+s = input("Enter string: ").title()
+print(s)
---
+++ homeworks/home_work_7/WordsInReverse.py
@@ -0,0 +1,8 @@
+#5. Написать программу, которая вернет строку со словами в обратном порядке
+#Например: "The greatest victory is that which requires no battle"
+#Ответ: "battle no requires which that is victory greatest The"
+#Строку ввести с клавиатуры
+
+s = input("Enter string: ").replace('"', "").split(" ")[::-1]
+s_lst =' '.join(str(i) for i in s)
+print(f'"{s_lst}"')
