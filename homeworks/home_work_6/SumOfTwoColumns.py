1d239fc64ff2b69a4ffd2e0349d6e130959e4076
Daria Loboiko <darloboyko@gmail.com>
2021-09-17 13:53:48 +0300

    py_courses

--- homeworks/home_work_6/SumOfTwoColumns.py
+++ homeworks/home_work_6/SumOfTwoColumns.py
@@ -3,11 +3,12 @@
 # | row_1 | row_2 | sum |
 # |  2 | 5 | 7 |
 
-row_1 = [1, 2, 5, 6]
-row_2 = [11, 5, 5, 7]
+row_1 = [1, 2, 5, 6, 54]
+row_2 = [11, 5, 5, 7, 3]
 
-if len(row_1) == len(row_2): 
-    print("|",sum(row_1),"|",sum(row_2),"|", sum(row_1)+sum(row_2), "|")
+if len(row_1) == len(row_2):
+    for i in range(0, len(row_1)): 
+        print("|",row_1[i],"|",row_2[i],"|", row_1[i]+row_2[i], "|")
 elif len(row_1) > len(row_2):
     print("The row_1 biggest")
 else:
