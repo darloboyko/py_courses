ed7963e4988fa29c5765e6a6f06b6a4e12fbd946
Daria Loboiko <darloboyko@gmail.com>
2021-09-17 12:22:07 +0300

    py_course

--- homeworks/home_work_8/MinimumElementReplacement.py
+++ homeworks/home_work_8/MinimumElementReplacement.py
@@ -2,15 +2,16 @@
 # Заменить минимальный элемент в этом списке на это среднее арифметическое
 
 
-lst = [1, -3, 4, 45, -36, 7, -95, 23, 11, -45, -13, 22, -35]
+lst = [1, -3, 4, 45, 47, 7, -55, 23, 11, -15, -13, 22, -35]
 new_list_neg = []
 for i in range(len(lst)):
-    if min(lst) == lst[i]:
-        lst[i] = mid_sum_lst
-        break
     if lst[i]<0:
         new_list_neg.append(lst[i])
-        mid_sum_lst = round((sum(new_list_neg)/len(new_list_neg)), 1)
+        mid_sum = round((sum(new_list_neg)/len(new_list_neg)), 1)
+        print(mid_sum)
+    if min(lst) == lst[i]:
+        lst[i] = mid_sum
+        break
 print(lst)
 
 
