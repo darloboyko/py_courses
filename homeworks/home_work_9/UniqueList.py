867a7d807c4ae529323c5ed1dd49a0d9318e0e9e
Daria Loboiko <darloboyko@gmail.com>
2021-09-21 09:51:58 +0300

    py_courses

--- homeworks/home_work_9/UniqueList.py
+++ homeworks/home_work_9/UniqueList.py
@@ -5,6 +5,7 @@
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
 a, b = set(a), set(b)
-unic_set = a&b
-print(list(unic_set))
+unique_set = a&b
+print(list(unique_set))
--- homeworks/home_work_9/UniqueNumber.py
+++ homeworks/home_work_9/UniqueNumber.py
@@ -1,15 +1,15 @@
 #3. Написать программу, которая на вход принимает список чисел и проверяет, 
 #все ли числа в этой последовательности уникальны
 
-n = int(input("Entel len list: "))
-i = 0
+import random
+
+i = 10
 lst_1 = []
 
-while i < n:
-    elem = int(input("Enter element: "))
-    lst_1.append(elem) 
-    i += 1
-#lst_1 = [12, 45, 34, 1, 2, 3, 4, 5, 89]
+while i > 0:
+    lst_1.append(random.randint(0, 100)) 
+    i -= 1
+print(lst_1)
 
 print("True") if len(set(lst_1)) == len(lst_1) else print("False")   
     
