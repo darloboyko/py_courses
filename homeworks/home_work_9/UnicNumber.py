bcaa0197dd17e1d1d8d0f497df82129196e53d6f
Daria Loboiko <darloboyko@gmail.com>
2021-09-21 09:30:51 +0300

    py_courses

--- homeworks/home_work_9/UnicNumber.py
+++ homeworks/home_work_9/UnicNumber.py
@@ -1,7 +1,15 @@
 #3. Написать программу, которая на вход принимает список чисел и проверяет, 
 #все ли числа в этой последовательности уникальны
 
-lst_1 = [12, 45, 34, 1, 2, 3, 4, 5, 89]
+n = int(input("Entel len list: "))
+i = 0
+lst_1 = []
+
+while i < n:
+    elem = int(input("Enter element: "))
+    lst_1.append(elem) 
+    i += 1
+#lst_1 = [12, 45, 34, 1, 2, 3, 4, 5, 89]
 
 print("True") if len(set(lst_1)) == len(lst_1) else print("False")   
     
